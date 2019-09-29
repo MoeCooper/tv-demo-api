3e3227be4c1decfcb44b2a8e4925d2d3eeb7c4e1
Maurice <42690947+MoeCooper@users.noreply.github.com>
2019-09-29 12:26:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const cors = require('cors')
-const fetch = require('node-fetch')
 const port = 3001;
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mcooper72:p%40ssword%21@cluster0-nez49.mongodb.net/test?retryWrites=true&w=majority";
+const instance = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true});
 
-let tvShowArray = [
-    {'show': 'bbt', 'year': '1992'},
-    {'show': 'ghostbusters', 'year': '1990'},
-    {'show': 'spongebob', 'year': '1994'}
-]
+// let tvShowArray = [
+//   {"firstname": "maurice",
+//     "lastname": "cooper"}
+// ]
 
 app.use(bodyParser.json())
-
-app.use((req, res,) => {
-    res.send(tvShowArray)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'application/json, content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 })
 
-app.route('/shows')
-  .get(function (req, res) {
-    res.status(200).send('File is good')
-    res.send('Get a tv show')
+
+instance.connect((err, client) => {
+  const collection = client.db("tv-demo-api").collection("tv-demo-api")
+  app.post('/tvDemo', (req, res)=> {
   })
-  .post(function (req, res) {
-    tvShowArray.push(req.body)
-    if(!tvShowArray){
-        res.status().send('Internal Server Error')
-    }
-    res.send('Add a tv show')
+  app.get('/tv-demo-api', async (req, res) => {
+     const data = await collection.find().toArray()
+     res.send(data)
+})
+
+app.get('/tv-demo-api', (req, res) => {
+  instance.connect((err, client) => {
+    if(err) res.send(err)
+    const collection = client.db("tv-demo-api").collection("tvdemo")
+    collection.find().toArray().then(r => res.send(r))
   })
-  .put(function (req, res) {
-    res.status(200).send('Ok')
-    res.send('Update the tv show')
-  })
-  .delete(function (req, res) {
-    res.status(200).send('Ok')        
-      res.send('deleted the tv show')
+})
+
+// app.route('/shows')
+//   .get(function (req, res) {
+//     res.status(200).send('File is good')
+//     res.send('Get a tv show')
+//   })
+//   .post(function (req, res) {
+//     tvShowArray.push(req.body)
+//     if(!tvShowArray){
+//         res.status().send('Internal Server Error')
+//     }
+//     res.send('Add a tv show')
+//   })
+//   .put(function (req, res) {
+//     res.status(200).send('Ok')
+//     res.send('Update the tv show')
+//   })
+//   .delete(function (req, res) {
+//     res.status(200).send('Ok')        
+//       res.send('deleted the tv show')
+//   })
+
+  app.get('/find...', (req, res) => {
+    instance.connect({_id: req.params.id}).then((data) => {
+      res.send(data)
+      db.close()
+    })
   })
 
-// app.get('/shows', (req, res) => {
-//     res.send(tvShowArray)
-// })
 
 // app.post('/shows', (req, res) => {
 //     tvShowArray.push(req.body)
@@ -57,5 +82,5 @@ app.route('/shows')
 //     res.send(tvShowArray)
 // })
 
-
+})
 app.listen(port, () => console.log('running on port 3001'))
